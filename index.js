427060492dcc89ebc3233fe43bbf722e0c797186
kennedy maina <kenneyrit@gmail.com>
2022-11-27 08:51:17 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 6
+let num4 = 10
+let random = 4
+
+let multiply = num1 * num2;
+
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+let mod = num4 % num3
+
+let max = Math.max(5, 10, 15, 20);
